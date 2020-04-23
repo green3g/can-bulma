32971ed4ae97d902e28d289d21091ad1833f22b6
roemhildtg <roemhildtg@gmail.com>
2020-04-22 21:19:07 -0500

    cleanup pagination

--- src/sp-admin/mixins/paginate.js
+++ src/sp-admin/mixins/paginate.js
@@ -1,27 +1,60 @@
-import get from 'can-util/js/get/get';
 
+/**
+ * @module sp-admin/mixins/paginate
+ * Pagination requires a total property to be updated after filtered queries are applied. This can be updated
+ * in several ways:
+ * 
+ * Use model parameters `model.metadata.total`:
+ *  - `metadata` - An object with a total property. This should get updated, whenever a query is made. 
+ * 
+ * Return a total property on the resolved object array:
+ * getList(){
+ *  const result = [];
+ *  result.total = 12;
+ *  return result
+ * }
+ */
 export default {
     total: {
-        get () {
-            return get(this, 'model.metadata.total') || 0;
+        value ({resolve, listenTo}) {
+            listenTo('objectsPromise', ((ev, promise) => {
+                if (!promise) {
+                    return;
+                }
+                promise.then((objects) => {
+                    if (typeof objects.total !== 'undefined') {
+                        resolve(objects.total);
+                    }
+                });
+            }));
+
+            listenTo('model.metadata.total', resolve);
         }
     },
     pageIndex: {
-        set (num) {
+        set (index) {
+            const count = this.perPage;
             this.params.page.assign({
-                start: num * this.perPage,
-                end: (num + 1) * this.perPage
+                start: this.getStart(index, count),
+                end: this.getEnd(index, count)
             });
-            return num;
+            return index;
         }
     },
     perPage: {
         set (count) {
+            const index = this.pageIndex;
             this.params.page.assign({
-                start: this.pageIndex * count,
-                end: (this.pageIndex + 1) * count
+                start: this.getStart(index, count),
+                end: this.getEnd(index, count)
             });
             return count;
         }
+    },
+    getStart (index, perPage) {
+        return index * perPage;
+    },
+    getEnd (index, perPage) {
+        return (index + 1) * perPage - 1;
     }
 };
